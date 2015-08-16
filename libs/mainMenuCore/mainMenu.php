e2e08000fc7ff303dfd390de6e088117fbbb97c0
B3BECb <danith@yandex.ru>
2015-08-16 15:49:23 +0400

    V0.4a
    начинаю заменять устаревшие mysql соеденения на mysqli

--- libs/mainMenuCore/mainMenu.php
+++ libs/mainMenuCore/mainMenu.php
@@ -1,5 +1,12 @@
 <?php
 	/**
+	* 
+	*/
+	class EmptyThemeName extends Exception
+	{		
+	}
+
+	/**
 	* Родительский класс пользователей git!
 	*/
 	class user
@@ -55,22 +62,51 @@
 		function getThemes()
 		{
 			//выбрать все темы, где владелец этот учитель
-			//mysql_query("SELECT * FROM ");
+			$teacherThemes = mysql_query("SELECT theme_id, themeName, discription, img FROM themes WHERE teacher_id_fk = 1");
+			if(!mysql_num_rows($teacherThemes)) return 0;
 
+			//$themes = mysql_fetch_array($teacherThemes, MYSQLI_ASSOC);
+			while($theme = mysql_fetch_array($teacherThemes))
+			{
+				echo '<div class="objBox">
+				<img class="objImg" src='.(($theme['img'])?('themes/theme_'.$theme['theme_id'].'/'.$theme['img']):'').'>
+				<div class="objName"> 
+					<span class="name">'.$theme['themeName'].'</span>
+				</div>	
+				<div class="objDiscription">
+					'.$theme['discription'].' 
+				</div>			
+				<div class="objControls">
+					<span class="topBtn">
+						<div class="controlButton" style="top:0px; position:relative;">
+							<span class="topButtonText">Изменить</span>
+							<img src="../../svgs/edit.svg">
+						</div>
+					</span>
+					<span class="topBtn">
+						<div id="theme_'.$theme['theme_id'].'" class="controlButton" style="top:0px; position:relative;">
+							<span class="topButtonText">Удалить</span>
+							<img src="../../svgs/delete.svg">
+						</div>
+					</span>
+				</div>
+			</div>';
+			}
 			//сформировать ячейки. удалить/изменить/аннотация. в ячейках выползающий див с названием. задний фон - картинка
 		}
 
 		function newTheme($themeName, $themeDiscription = '', $themeIMG = '')
-		{
-			//заносим новую тему в БД
-			mysql_query("INSERT INTO themes values (null, '$themeName', $this->id, '$themeDiscription', '".$themeIMG['name']."')");
-			$lastInsertId = mysql_insert_id();
-
-			//Создать новую директорию темы
-			mkdir("themes/theme_$lastInsertId");
-
+		{			
 			try
 			{
+				if (empty($themeName)) throw new EmptyThemeName("Недопустимое название темы.");
+				//заносим новую тему в БД
+				mysql_query("INSERT INTO themes values (null, '$themeName', $this->id, '$themeDiscription', '".$themeIMG['name']."')");
+				$lastInsertId = mysql_insert_id();
+
+				//Создать новую директорию темы
+				mkdir("themes/theme_$lastInsertId");
+
 				if ($themeIMG['tmp_name'])
 				{
 					$ExtentionsClassificator = new extensionClassificator();
@@ -85,12 +121,16 @@
 
 				} else $success = 1;
 			}
+			catch (EmptyThemeName $e)
+			{
+				$this->jsOnResponse("{'message':'Ошибка создания темы! ".$e->getMessage()."', 'success':'0'}");
+			}
 			catch (Exception $e)
 			{
 				$this->removeDirectory("themes/theme_$lastInsertId");
 				mysql_query("DELETE FROM themes WHERE theme_id = $lastInsertId;");
 
-				$this->jsOnResponse("{'message':'Тема не создана! ".$e->getMessage()."', 'success':'0'}");
+				$this->jsOnResponse("{'message':'Ошибка создания темы! ".$e->getMessage()."', 'success':'0'}");
 			}						
 
 			if ($success) $this->jsOnResponse("{'message':'Тема создана.', 'success':'1', 'teacherId':'" . $this->id . "', 'themeName':'$themeName', 'themeDiscription':'$themeDiscription', 'themeIMG':'$file'}");
